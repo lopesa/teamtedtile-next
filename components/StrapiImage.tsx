0da30511e90f1f16ebec13f5a2243f1d40644c2e
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-01-23 15:58:44 -0400

    improves StrapiImage image size selection and adds a fallback in case
    the generated url doesn't exist. In that case use the original source image

--- components/StrapiImage.tsx
+++ components/StrapiImage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 interface StrapiProps {
   src: string;
@@ -8,45 +9,76 @@ interface StrapiProps {
 
 interface NextImageProps {}
 
-const widthTranlations = {
+// these values are mirrored in the Strapi backend
+// located @ /plugins.ts
+const widthTranslations = {
   500: "small_",
   750: "medium_",
   1000: "large_",
+  2000: "xlarge_",
+  4000: "xxlarge_",
 };
 
 const getWidthTranslation = (width: number) => {
   if (width <= 500) {
-    return widthTranlations[500];
+    return widthTranslations[500];
+  } else if (width > 500 && width <= 750) {
+    return widthTranslations[750];
+  } else if (width > 750 && width <= 1000) {
+    return widthTranslations[1000];
+  } else if (width > 1000 && width <= 2000) {
+    return widthTranslations[2000];
+  } else {
+    return widthTranslations[4000];
   }
-  if (width > 500 && width <= 750) {
-    return widthTranlations[750];
-  }
-  if (width <= 1000) {
-    return widthTranlations[1000];
-  }
-  return "";
 };
 
-const myLoader = ({ src, width }: StrapiProps) => {
+const sizedImageLoader = ({ src, width }: StrapiProps) => {
   let index = src.search(/uploads/) + 8;
   const url = `${src.substring(0, index)}${getWidthTranslation(
     width
   )}${src.substring(index)}`;
   return url;
-  //   return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+  // DEAFAULT === return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+};
+
+const backupLoader = (props: StrapiProps | undefined) => {
+  return props?.src || "";
 };
 
 const StrapiImage = ({ src, alt, width, height, fill, sizes }: any) => {
+  const [useSizedImage, setUseSizedImage] = useState(true);
+  const [useBaseImage, setUseBaseImage] = useState(false);
+  const handleError = () => {
+    setUseSizedImage(false);
+    setUseBaseImage(true);
+  };
   return (
-    <Image
-      loader={myLoader}
-      src={src}
-      alt={alt}
-      fill={fill}
-      width={width}
-      height={height}
-      sizes={sizes}
-    />
+    <>
+      {useSizedImage && (
+        <Image
+          loader={sizedImageLoader}
+          src={src}
+          onError={handleError}
+          alt={alt}
+          fill={fill}
+          width={width}
+          height={height}
+          sizes={sizes}
+        />
+      )}
+      {useBaseImage && (
+        <Image
+          loader={backupLoader}
+          src={src}
+          alt={alt}
+          fill={fill}
+          width={width}
+          height={height}
+          sizes={sizes}
+        />
+      )}
+    </>
   );
 };
 
