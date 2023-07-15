ba047098e235e29e33cfdcfacc4d107e98628923
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-07-15 13:18:19 -0400

    removes the gallery routes from APP dir and puts them back into PAGES dir
    - the gallery slideshow images no longer stutter, just with the simple swap

---
+++ pages/gallery/index.tsx
@@ -0,0 +1,65 @@
+import { GetStaticProps } from "next";
+import HomeSplash from "components/homeSplash";
+import Menu from "components/menu";
+import ScrollGallery from "components/ScrollGallery";
+import { IGalleryItemsResponse, IGalleryItem } from "interfaces/gallery";
+import { getApiUrlBase } from "utils/GeneralUtils";
+import { RefObject, useRef, useState } from "react";
+import BusinessCard from "components/BusinessCard";
+// import PageHead from "components/PageHead";
+
+/**
+ * this page and the index are duped code mostly but I am seeing that
+ * it's not wise to refactor out the getStaticProps call to a common place
+ * think about just factoring out the core of it later
+ */
+
+interface HomeGalleryProps {
+  images: IGalleryItem[];
+  notFound: boolean;
+}
+
+export default function HomeGallery({ images, notFound }: HomeGalleryProps) {
+  const scrollGalleryRef = useRef<HTMLElement>(null);
+  const scrollGalleryRefIntoView = (
+    scrollGalleryRefPass: RefObject<HTMLElement>
+  ) => {
+    scrollGalleryRefPass?.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  const [menuScrolledPast, setMenuScrolledPast] = useState(false);
+  return (
+    <>
+      {/* <PageHead metaContent="Gallery" /> */}
+
+      <Menu position="fixed" />
+      <main>
+        {/* <HomeSplash setIsScrolledPastHomeSplash={setMenuScrolledPast} /> */}
+        <HomeSplash />
+        <ScrollGallery
+          ref={scrollGalleryRef}
+          images={images}
+          notFound={notFound}
+          onLayoutSetMethods={[
+            scrollGalleryRefIntoView.bind(HomeGallery, scrollGalleryRef),
+          ]}
+        />
+        <BusinessCard />
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (): Promise<{
+  props: HomeGalleryProps;
+}> => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/fetch
+  const res = await fetch(`${getApiUrlBase()}/api/gallery-images`).catch(
+    (e) => {}
+  );
+  if (res && res.ok) {
+    const json: IGalleryItemsResponse = await res.json();
+    return { props: { images: json.data, notFound: false } };
+  } else {
+    return { props: { images: [], notFound: true } };
+  }
+};
